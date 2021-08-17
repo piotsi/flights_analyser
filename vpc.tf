6cdec24b7ac9aa8333aa285db1cc46218058f78c
Piotr Sikora <piotrsikoraaa@gmail.com>
2021-08-17 23:44:59 +0200

    Update versions

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ module "vpc" {
   azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
-#   msk_subnets     = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
+  #   msk_subnets     = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
