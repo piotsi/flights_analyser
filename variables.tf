3770a5bcb7b6c108e58f19bdc63a9ba5222110aa
Piotr Sikora <piotrsikoraaa@gmail.com>
2021-08-17 23:38:35 +0200

    Use module for vpc creation

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  type    = string
+  default = ""
+}
+
+variable "vpc_main_cidr" {
+  type    = string
+  default = ""
+}
---
+++ vpc.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "vpc_main"
+  cidr = var.vpc_main_cidr
+
+  azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+#   msk_subnets     = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  vpc_tags = {
+    Name = "vpc_main"
+  }
+}
